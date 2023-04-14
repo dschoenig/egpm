392b43d0f280ba69a7fb7c4830d199836828f6b1
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-04-14 12:33:45 -0400

    Test landscape sim, again

--- src/test_ls.sh
+++ src/test_ls.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=16G
-#SBATCH --time=0:15:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5G
+#SBATCH --time=2:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
 module load StdEnv/2020 gcc/9.3.0 gdal/3.5.1 geos/3.10.2 python/3.10 udunits/2.2.28 r/4.2.2
 
-Rscript 1_landscapes.R imbalance_medium 2 1000 0.3 1 1 $SLURM_CPUS_PER_TASK
+Rscript 1_landscapes.R imbalance_medium 1000 1000 0.3 1 100
