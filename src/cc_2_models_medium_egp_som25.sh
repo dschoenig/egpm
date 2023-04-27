63327e76500cf8137e1ad38d21d29c2d7bada84e
Daniel Schoenig <schoed@narval4.narval.calcul.quebec>
2023-04-27 14:46:38 -0400

    Memory allocation

--- src/cc_2_models_medium_egp_som25.sh
+++ src/cc_2_models_medium_egp_som25.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=5G
+#SBATCH --mem=8G
 #SBATCH --time=1:30:00
 #SBATCH --array=1-100
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
