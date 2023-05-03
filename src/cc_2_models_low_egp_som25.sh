c56f1f664d6fede845c45d0538eaf1ada0d61a41
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-05-03 14:57:11 -0400

    EGP tests

--- src/cc_2_models_low_egp_som25.sh
+++ src/cc_2_models_low_egp_som25.sh
@@ -3,9 +3,10 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=8G
 #SBATCH --time=1:30:00
+#SBATCH --array=1-100
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
 module load StdEnv/2020 gcc/9.3.0 gdal/3.5.1 geos/3.10.2 python/3.10 udunits/2.2.28 r/4.2.2
 
-Rscript 2_egp_som25.R imbalance_high egp_som25 $SLURM_CPUS_PER_TASK 1 100
+Rscript 2_egp_som25.R imbalance_low egp_som25 $SLURM_CPUS_PER_TASK $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
