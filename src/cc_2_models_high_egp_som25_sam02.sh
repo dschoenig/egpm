bd759303b606cbb1e36e5d075bae7af6b47be9ea
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-05-18 15:37:11 -0400

    Resource allocation

--- src/cc_2_models_high_egp_som25_sam02.sh
+++ src/cc_2_models_high_egp_som25_sam02.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=8G
+#SBATCH --mem=12G
 #SBATCH --time=3:00:00
 #SBATCH --array=1-200
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
