2cb1b0bb9e022a6d42d53b1b7592eee7e1a33b7d
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-05-19 11:36:39 -0400

    Remove test line

--- src/cc_1_landscapes_high.sh
+++ src/cc_1_landscapes_high.sh
@@ -10,4 +10,3 @@
 module load StdEnv/2020 gcc/9.3.0 gdal/3.5.1 geos/3.10.2 python/3.10 udunits/2.2.28 r/4.2.2
 
 Rscript 1_landscapes.R imbalance_high 1000 1000 0.3 $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
-Rscript 1_landscapes.R test 1 100 0.3 1 1 4
