81e5bd0b625b32d24815d0fd545a20d7dace8068
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-08-09 10:31:19 -0400

    brm: more models, intercept and factor coding

--- src/cc_5_compare_9.sh
+++ src/cc_5_compare_9.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=8:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=brm5
+#SBATCH --job-name=brm9
 
 module load StdEnv/2020 gcc/9.3.0 gdal/3.5.1 geos/3.10.2 python/3.10 udunits/2.2.28 r/4.2.2
 
-Rscript 5_compare_brm.R 5
+Rscript 5_compare_brm.R 9
