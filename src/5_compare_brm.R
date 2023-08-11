7d1939ca2bbc1818282ce5115353cb1af4440004
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-08-11 13:43:39 -0400

    brm: model nu, but only according to ls type

--- src/5_compare_brm.R
+++ src/5_compare_brm.R
@@ -682,6 +682,40 @@ if(mod.id == 18) {
 }
 
 
+if(mod.id == 19) {
+
+  priors <- c(
+              prior(student_t(3, 1, 1), class = Intercept),
+              prior(student_t(3, 0, 1), class = b),
+              prior(student_t(3, 0, 1), class = sd),
+              prior(student_t(3, 2, 1), class = Intercept, dpar = nu),
+              prior(student_t(3, 0, 1), class = b, dpar = nu),
+              prior(student_t(3, 0, 1), class = Intercept, dpar = sigma),
+              prior(student_t(3, 0, 1), class = b, dpar = sigma),
+              prior(student_t(3, 0, 1), class = sd, dpar = sigma))
+
+  mod.form <-
+    bf(mar.std ~ name.short + (1 |l| ls.uid),
+       nu ~ ls.response*ls.imbalance,
+       sigma ~ name.short + (1 |l| ls.uid))
+
+  mod.mar <- brm(mod.form,
+                 family = student(),
+                 prior = priors,
+                 data = estimates.fit,
+                 chains = 4,
+                 cores = 4,
+                 threads = 4,
+                 warmup = 10000,
+                 iter = 20000,
+                 # save_pars = save_pars(all = TRUE),
+                 # init = 0,
+                 thin = 4,
+                 # control = list(adapt_delta = 0.7),
+                 refresh = 25,
+                 empty = FALSE)
+
+}
 
 saveRDS(mod.mar, file.mod)
 
