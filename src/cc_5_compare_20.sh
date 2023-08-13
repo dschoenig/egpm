b79eb84fbbc56d59f18ed5c5a5225f50f17d237c
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-08-13 16:21:14 -0400

    brm: try 8 chains

--- src/cc_5_compare_20.sh
+++ src/cc_5_compare_20.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=24G
-#SBATCH --time=48:00:00
+#SBATCH --time=96:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 #SBATCH --job-name=brm20
