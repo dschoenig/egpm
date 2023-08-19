ab7a6db8f6612aae2b445392b237dd701944cb79
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-08-19 16:09:52 -0400

    New EGP models for sensitivity

---
+++ src/cc_2_models_tweedie_high_egp_som25_sam005.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=def-cricrime 
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=12G
+#SBATCH --time=2:00:00
+#SBATCH --array=1-100
+#SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
+#SBATCH --mail-type=ALL
+
+module load StdEnv/2020 gcc/9.3.0 gdal/3.5.1 geos/3.10.2 python/3.10 udunits/2.2.28 r/4.2.2
+
+Rscript 2_egp.R tweedie_imbalance_high egp_som25_sam005 tweedie \
+	0.005 25 1000 250 250 TRUE \
+	$SLURM_CPUS_PER_TASK $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
---
+++ src/newmods.sh
@@ -0,0 +1,148 @@
+# EGP 10 all landscapes
+## Normal 
+cp cc_2_models_high_egp_som10.sh cc_2_models_low_egp_som10.sh
+sed -i "s/imbalance_high/imbalance_low/" cc_2_models_low_egp_som10.sh 
+sed -i "s/normal/normal/" cc_2_models_low_egp_som10.sh 
+
+## Binary
+cp cc_2_models_high_egp_som10.sh cc_2_models_binary_low_egp_som10.sh
+sed -i "s/imbalance_high/binary_imbalance_low/" cc_2_models_binary_low_egp_som10.sh 
+sed -i "s/normal/binary/" cc_2_models_binary_low_egp_som10.sh 
+cp cc_2_models_high_egp_som10.sh cc_2_models_binary_high_egp_som10.sh
+sed -i "s/imbalance_high/binary_imbalance_high/" cc_2_models_binary_high_egp_som10.sh 
+sed -i "s/normal/binary/" cc_2_models_binary_high_egp_som10.sh 
+
+## Tweedie
+cp cc_2_models_high_egp_som10.sh cc_2_models_tweedie_low_egp_som10.sh
+sed -i "s/imbalance_high/tweedie_imbalance_low/" cc_2_models_tweedie_low_egp_som10.sh 
+sed -i "s/normal/tweedie/" cc_2_models_tweedie_low_egp_som10.sh 
+cp cc_2_models_high_egp_som10.sh cc_2_models_tweedie_high_egp_som10.sh
+sed -i "s/imbalance_high/tweedie_imbalance_high/" cc_2_models_tweedie_high_egp_som10.sh 
+sed -i "s/normal/tweedie/" cc_2_models_tweedie_high_egp_som10.sh 
+
+
+# EGP 50 all landscapes
+## Normal 
+cp cc_2_models_high_egp_som50.sh cc_2_models_low_egp_som50.sh
+sed -i "s/imbalance_high/imbalance_low/" cc_2_models_low_egp_som50.sh 
+sed -i "s/normal/normal/" cc_2_models_low_egp_som50.sh 
+
+## Binary
+cp cc_2_models_high_egp_som50.sh cc_2_models_binary_low_egp_som50.sh
+sed -i "s/imbalance_high/binary_imbalance_low/" cc_2_models_binary_low_egp_som50.sh 
+sed -i "s/normal/binary/" cc_2_models_binary_low_egp_som50.sh 
+cp cc_2_models_high_egp_som50.sh cc_2_models_binary_high_egp_som50.sh
+sed -i "s/imbalance_high/binary_imbalance_high/" cc_2_models_binary_high_egp_som50.sh 
+sed -i "s/normal/binary/" cc_2_models_binary_high_egp_som50.sh 
+
+## Tweedie
+cp cc_2_models_high_egp_som50.sh cc_2_models_tweedie_low_egp_som50.sh
+sed -i "s/imbalance_high/tweedie_imbalance_low/" cc_2_models_tweedie_low_egp_som50.sh 
+sed -i "s/normal/tweedie/" cc_2_models_tweedie_low_egp_som50.sh 
+cp cc_2_models_high_egp_som50.sh cc_2_models_tweedie_high_egp_som50.sh
+sed -i "s/imbalance_high/tweedie_imbalance_high/" cc_2_models_tweedie_high_egp_som50.sh 
+sed -i "s/normal/tweedie/" cc_2_models_tweedie_high_egp_som50.sh 
+
+
+# EGP 25 unequal all landscapes
+## Normal 
+cp cc_2_models_high_egp_som25_unequal.sh cc_2_models_low_egp_som25_unequal.sh
+sed -i "s/imbalance_high/imbalance_low/" cc_2_models_low_egp_som25_unequal.sh 
+sed -i "s/normal/normal/" cc_2_models_low_egp_som25_unequal.sh 
+
+## Binary
+cp cc_2_models_high_egp_som25_unequal.sh cc_2_models_binary_low_egp_som25_unequal.sh
+sed -i "s/imbalance_high/binary_imbalance_low/" cc_2_models_binary_low_egp_som25_unequal.sh 
+sed -i "s/normal/binary/" cc_2_models_binary_low_egp_som25_unequal.sh 
+cp cc_2_models_high_egp_som25_unequal.sh cc_2_models_binary_high_egp_som25_unequal.sh
+sed -i "s/imbalance_high/binary_imbalance_high/" cc_2_models_binary_high_egp_som25_unequal.sh 
+sed -i "s/normal/binary/" cc_2_models_binary_high_egp_som25_unequal.sh 
+
+## Tweedie
+cp cc_2_models_high_egp_som25_unequal.sh cc_2_models_tweedie_low_egp_som25_unequal.sh
+sed -i "s/imbalance_high/tweedie_imbalance_low/" cc_2_models_tweedie_low_egp_som25_unequal.sh 
+sed -i "s/normal/tweedie/" cc_2_models_tweedie_low_egp_som25_unequal.sh 
+cp cc_2_models_high_egp_som25_unequal.sh cc_2_models_tweedie_high_egp_som25_unequal.sh
+sed -i "s/imbalance_high/tweedie_imbalance_high/" cc_2_models_tweedie_high_egp_som25_unequal.sh 
+sed -i "s/normal/tweedie/" cc_2_models_tweedie_high_egp_som25_unequal.sh 
+
+
+# EGP large sample all landscapes
+## Normal 
+cp cc_2_models_high_egp_som25_sam02.sh cc_2_models_low_egp_som25_sam02.sh
+sed -i "s/imbalance_high/imbalance_low/" cc_2_models_low_egp_som25_sam02.sh 
+sed -i "s/normal/normal/" cc_2_models_low_egp_som25_sam02.sh 
+
+## Binary
+cp cc_2_models_high_egp_som25_sam02.sh cc_2_models_binary_low_egp_som25_sam02.sh
+sed -i "s/imbalance_high/binary_imbalance_low/" cc_2_models_binary_low_egp_som25_sam02.sh 
+sed -i "s/normal/binary/" cc_2_models_binary_low_egp_som25_sam02.sh 
+cp cc_2_models_high_egp_som25_sam02.sh cc_2_models_binary_high_egp_som25_sam02.sh
+sed -i "s/imbalance_high/binary_imbalance_high/" cc_2_models_binary_high_egp_som25_sam02.sh 
+sed -i "s/normal/binary/" cc_2_models_binary_high_egp_som25_sam02.sh 
+
+## Tweedie
+cp cc_2_models_high_egp_som25_sam02.sh cc_2_models_tweedie_low_egp_som25_sam02.sh
+sed -i "s/imbalance_high/tweedie_imbalance_low/" cc_2_models_tweedie_low_egp_som25_sam02.sh 
+sed -i "s/normal/tweedie/" cc_2_models_tweedie_low_egp_som25_sam02.sh 
+cp cc_2_models_high_egp_som25_sam02.sh cc_2_models_tweedie_high_egp_som25_sam02.sh
+sed -i "s/imbalance_high/tweedie_imbalance_high/" cc_2_models_tweedie_high_egp_som25_sam02.sh 
+sed -i "s/normal/tweedie/" cc_2_models_tweedie_high_egp_som25_sam02.sh
+
+
+# EGP small sample all landscapes
+## Normal 
+cp cc_2_models_high_egp_som25_sam005.sh cc_2_models_low_egp_som25_sam005.sh
+sed -i "s/imbalance_high/imbalance_low/" cc_2_models_low_egp_som25_sam005.sh 
+sed -i "s/normal/normal/" cc_2_models_low_egp_som25_sam005.sh 
+
+## Binary
+cp cc_2_models_high_egp_som25_sam005.sh cc_2_models_binary_low_egp_som25_sam005.sh
+sed -i "s/imbalance_high/binary_imbalance_low/" cc_2_models_binary_low_egp_som25_sam005.sh 
+sed -i "s/normal/binary/" cc_2_models_binary_low_egp_som25_sam005.sh 
+cp cc_2_models_high_egp_som25_sam005.sh cc_2_models_binary_high_egp_som25_sam005.sh
+sed -i "s/imbalance_high/binary_imbalance_high/" cc_2_models_binary_high_egp_som25_sam005.sh 
+sed -i "s/normal/binary/" cc_2_models_binary_high_egp_som25_sam005.sh 
+
+## Tweedie
+cp cc_2_models_high_egp_som25_sam005.sh cc_2_models_tweedie_low_egp_som25_sam005.sh
+sed -i "s/imbalance_high/tweedie_imbalance_low/" cc_2_models_tweedie_low_egp_som25_sam005.sh 
+sed -i "s/normal/tweedie/" cc_2_models_tweedie_low_egp_som25_sam005.sh 
+cp cc_2_models_high_egp_som25_sam005.sh cc_2_models_tweedie_high_egp_som25_sam005.sh
+sed -i "s/imbalance_high/tweedie_imbalance_high/" cc_2_models_tweedie_high_egp_som25_sam005.sh 
+sed -i "s/normal/tweedie/" cc_2_models_tweedie_high_egp_som25_sam005.sh 
+
+
+sbatch cc_2_models_low_egp_som10.sh
+sbatch cc_2_models_binary_low_egp_som10.sh
+sbatch cc_2_models_binary_high_egp_som10.sh
+sbatch cc_2_models_tweedie_low_egp_som10.sh
+sbatch cc_2_models_tweedie_high_egp_som10.sh
+
+
+sbatch cc_2_models_low_egp_som50.sh
+sbatch cc_2_models_binary_low_egp_som50.sh
+sbatch cc_2_models_binary_high_egp_som50.sh
+sbatch cc_2_models_tweedie_low_egp_som50.sh
+sbatch cc_2_models_tweedie_high_egp_som50.sh
+
+
+sbatch cc_2_models_low_egp_som25_unequal.sh
+sbatch cc_2_models_binary_low_egp_som25_unequal.sh
+sbatch cc_2_models_binary_high_egp_som25_unequal.sh
+sbatch cc_2_models_tweedie_low_egp_som25_unequal.sh
+sbatch cc_2_models_tweedie_high_egp_som25_unequal.sh
+
+
+sbatch cc_2_models_low_egp_som25_sam02.sh
+sbatch cc_2_models_binary_low_egp_som25_sam02.sh
+sbatch cc_2_models_binary_high_egp_som25_sam02.sh
+sbatch cc_2_models_tweedie_low_egp_som25_sam02.sh
+sbatch cc_2_models_tweedie_high_egp_som25_sam02.sh
+
+
+sbatch cc_2_models_low_egp_som25_sam005.sh
+sbatch cc_2_models_binary_low_egp_som25_sam005.sh
+sbatch cc_2_models_binary_high_egp_som25_sam005.sh
+sbatch cc_2_models_tweedie_low_egp_som25_sam005.sh
+sbatch cc_2_models_tweedie_high_egp_som25_sam005.sh
