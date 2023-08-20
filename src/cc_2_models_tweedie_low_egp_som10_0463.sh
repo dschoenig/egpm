09d82b0939983c9f0eb90046ca6ea012dc353e44
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-08-20 12:21:42 -0400

    Again: Fix missing SOM10 models

--- src/cc_2_models_tweedie_low_egp_som10_0463.sh
+++ src/cc_2_models_tweedie_low_egp_som10_0463.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=12G
-#SBATCH --time=0:30:00
+#SBATCH --time=3:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
@@ -10,4 +10,4 @@ module load StdEnv/2020 gcc/9.3.0 gdal/3.5.1 geos/3.10.2 python/3.10 udunits/2.2
 
 Rscript 2_egp.R tweedie_imbalance_low egp_som10 tweedie \
 	0.01 10 1000 100 250 TRUE \
-	$SLURM_CPUS_PER_TASK 463 1000
+	$SLURM_CPUS_PER_TASK 47 100
