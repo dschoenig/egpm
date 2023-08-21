dba23de2ee0c8b5ebefa09ac6c8c4dae8c874a7e
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-08-21 08:19:45 -0400

    Marginals SOM50

---
+++ src/cc_3_marginal_low_egp_som50.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=def-cricrime 
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=12G
+#SBATCH --time=4:00:00
+#SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
+#SBATCH --mail-type=ALL
+
+module load StdEnv/2020 gcc/9.3.0 gdal/3.5.1 geos/3.10.2 python/3.10 udunits/2.2.28 r/4.2.2
+
+Rscript 3_marginal_egp.R imbalance_low egp_som50
