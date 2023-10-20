72d9eceb76d60e1806755e9aa31c1d6b6ba92a56
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-10-20 18:59:21 -0400

    Fix typo

--- src/cc_1_landscapes_noeff_low_tweedie.sh
+++ src/cc_1_landscapes_noeff_low_tweedie.sh
@@ -9,4 +9,4 @@
 
 module load StdEnv/2020 gcc/9.3.0 gdal/3.5.1 geos/3.10.2 python/3.10 udunits/2.2.28 r/4.3.1
 
-Rscript 1_landscapes_tweedie.R noef_imbalance_low noeff_tweedie_imbalance_low $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
+Rscript 1_landscapes_tweedie.R noeff_imbalance_low noeff_tweedie_imbalance_low $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
